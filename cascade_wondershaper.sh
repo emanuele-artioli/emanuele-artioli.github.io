67436a3fa17e6ceee17e2643a64ebef297059e17
emanuele-artioli <122871538+emanuele-artioli@users.noreply.github.com>
2024-01-25 11:33:23 +0100

    removed new stuff

---
+++ cascade_wondershaper.sh
@@ -0,0 +1,48 @@
+# !/bin/bash
+sudo wondershaper-master/wondershaper -a enp0s31f6 -c
+sudo wondershaper-master/wondershaper -a enp0s31f6 -d 20000 -u 20000
+sleep 30
+
+sudo wondershaper-master/wondershaper -a enp0s31f6 -c
+sudo wondershaper-master/wondershaper -a enp0s31f6 -d 8000 -u 20000
+sleep 30
+
+sudo wondershaper-master/wondershaper -a enp0s31f6 -c
+sudo wondershaper-master/wondershaper -a enp0s31f6 -d 4000 -u 20000
+sleep 30
+
+sudo wondershaper-master/wondershaper -a enp0s31f6 -c
+sudo wondershaper-master/wondershaper -a enp0s31f6 -d 2000 -u 20000
+sleep 30
+
+sudo wondershaper-master/wondershaper -a enp0s31f6 -c
+sudo wondershaper-master/wondershaper -a enp0s31f6 -d 4000 -u 20000
+sleep 30
+
+sudo wondershaper-master/wondershaper -a enp0s31f6 -c
+sudo wondershaper-master/wondershaper -a enp0s31f6 -d 8000 -u 20000
+sleep 30
+
+sudo wondershaper-master/wondershaper -a enp0s31f6 -c
+sudo wondershaper-master/wondershaper -a enp0s31f6 -d 20000 -u 20000
+sleep 30
+
+sudo wondershaper-master/wondershaper -a enp0s31f6 -c
+sudo wondershaper-master/wondershaper -a enp0s31f6 -d 8000 -u 20000
+sleep 30
+
+sudo wondershaper-master/wondershaper -a enp0s31f6 -c
+sudo wondershaper-master/wondershaper -a enp0s31f6 -d 4000 -u 20000
+sleep 30
+
+sudo wondershaper-master/wondershaper -a enp0s31f6 -c
+sudo wondershaper-master/wondershaper -a enp0s31f6 -d 2000 -u 20000
+sleep 30
+
+sudo wondershaper-master/wondershaper -a enp0s31f6 -c
+sudo wondershaper-master/wondershaper -a enp0s31f6 -d 4000 -u 20000
+sleep 30
+
+sudo wondershaper-master/wondershaper -a enp0s31f6 -c
+sudo wondershaper-master/wondershaper -a enp0s31f6 -d 8000 -u 20000
+sleep 30
